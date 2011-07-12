f7cef27b2a199ed1555fde4346e24da50c9b50ac
The Night Phoenix <the-nightphoenix@U-Phoenix.(none)>
2011-07-12 16:46:24 +0200

    repaired player movment and screen blit

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ def main():
     MAX_BULLETS = 10
     MAX_INVADERS = 4
     PLAYER_OFFSET = 25
-    PLAYER_SPEED = 10
+    PLAYER_SPEED = 50
     game_loop = True
     
     pygame.init()
@@ -16,13 +16,18 @@ def main():
     screen = pygame.display.set_mode(size)
     
     icon = pygame.image.load('../gfx/icon.png')
-    invader1 = pygame.image.load('../gfx/invader1.png')
-    invader2 = pygame.image.load('../gfx/invader2.png')
-    bullet = pygame.image.load('../gfx/bullet1.png')
+    invader1 = pygame.image.load('../gfx/invader1.png').convert()
+    invader2 = pygame.image.load('../gfx/invader2.png').convert()
+    bullet = pygame.image.load('../gfx/bullet1.png').convert()
+    
+    #this background should be replaced by the background image
+    background = pygame.surface.Surface((size),0,None)
+    background.fill((130,21,138))
+    
+    screen.blit(background,background.get_rect())
     
     pygame.display.set_icon(icon)
-    pygame.display.set_caption('pyInvaders')
-    
+    pygame.display.set_caption('PyInvaders')
     
     bullets = []
     for i in range(MAX_BULLETS):
@@ -32,30 +37,39 @@ def main():
     for i in range(MAX_BULLETS):
         invaders.append(copy.deepcopy(invader1))
         invaders.append(copy.deepcopy(invader2))
-        
-    #p = player.Player(0,0)
+    
+    
+    
     p = player.Player(width,height,PLAYER_OFFSET,PLAYER_SPEED,screen)
     
-    while game_loop :
-       
+    while game_loop :       
+        
+        #clear old position
+        screen.blit(background,p.rect)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT : game_loop = not game_loop
             
+            
+            
             if event.type == pygame.KEYDOWN :
                 if event.key == pygame.K_ESCAPE:game_loop = not game_loop
                 
+                
                 if event.key == pygame.K_LEFT: 
-                    screen.fill((0,0,0))
-                    p.rect.move_ip(-PLAYER_SPEED,0)
-                    
+                    #p.rect.move_ip(-PLAYER_SPEED,0)
+                    p.move(0)
                 if event.key == pygame.K_RIGHT: 
-                    screen.fill((0,0,0))
-                    p.rect.move_ip(PLAYER_SPEED,0)
-                
-                
-        clock.tick(FPS)    
+                    #p.rect.move_ip(PLAYER_SPEED,1)
+                    p.move(1)
+
+        
+
+        #draw in the new position
         screen.blit(p.img,p.rect)
+        
+        clock.tick(FPS)    
+        pygame.display.update()
         pygame.display.flip()
     pygame.quit()
     
--- src/player.py
+++ src/player.py
@@ -1,30 +1,29 @@
 import pygame
 
-class Player(pygame.sprite.Sprite):
+class Player():
     def __init__(self,width,height,offset,Speed,Screen):
 
-        pygame.sprite.Sprite.__init__(self)
-        
-        self.img =  pygame.image.load('../gfx/player.png')
-        self.img.set_colorkey ((255,0,255))
-        self.rect = self.img.get_rect(topleft = (width/2,height-offset))
-        
-        self.x_pos = width/2
-        self.y_pos = height - offset
+        #pygame.sprite.Sprite.__init__(self)
+                
+        #self.x_pos = width/2
+        #self.y_pos = height - offset
         self.speed = Speed
         
         self.window_width = width
         self.window_height = height
-        self.screen = Screen
-    def update(self,direction):
+        
+        self.img =  pygame.image.load('../gfx/player.png')
+        self.img.set_colorkey ((255,0,255))
+        
+        self.rect = self.img.get_rect().move(width/2,height - offset)
+        
+        
+    def move(self,direction):
         #1 = right
         #0 = left
-        if direction : 
-            self.x_pos += self.speed
-        else :
-            self.x_pos -= self.speed
+        if direction : self.rect.move_ip(self.speed,0)
+        else : self.rect.move_ip(-self.speed,0)
         
-        if self.x_pos < 0 : self.x_pos = 0
-        if self.x_pos >self.window_width : self.xpos = self.window_width
+        if self.rect.left < 0 : self.rect.left = 0
+        if self.rect.right >self.window_width : self.rect.right = self.window_width
         
-        self.rect.clamp_ip(self.screen.get_rect())
